4b2a430d4702ec835191ab086586c6d3258599ba
Marcos Santiago <marcos14952010@hotmail.com>
2017-06-29 02:28:32 -0300

    files ok

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# novavida-php-api
+# NovaVidaTI - API Consulta CPF e CNPJ.
+
+## Requirements:
+- PHP 7.0 (or higher)
+- Composer
+
+## Installation:
+
+Follow the following steps:
+
+```bash
+composer install
+```
+
+## How To Use:
+
+Open your browser and use the url to perform the data search.
+
+```bash
+http://you_url/api/?type=cpf&document=11111111111 - For CPF
+http://you_url/api/?type=cnpj&document=11111111111111 - For CNPJ
+```
+
+## Configuration:
+
+All sensible data configuration is located at config/config.php file , the file must be something like:
+```ini
+$user = "USUARIO";
+$pass = "SENHA";
+$customer = "CLIENTE";
+```
---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+
+require 'vendor/autoload.php';
+require 'helpers/commom.php';
+require 'config/config.php';
+
+use GuzzleHttp\Client;
+
+$client = new GuzzleHttp\Client();
+
+if($type == 'cpf'){
+
+    $res = $client->request('GET', 'http://wsnv.novavidati.com.br/WSLocalizador.asmx/GerarToken?', [
+        'query'=> ['usuario'=> $user, 'senha' => $pass , 'cliente' => $customer]
+    ]);
+
+    $token = replaceWords($res->getBody());
+
+    $document = $client->request('GET', 'http://wsnv.novavidati.com.br/WSLocalizador.asmx/PessoasEmpresasTk?', [
+        'query'=> ['documento'=> $document_number, 'token' => $token]
+    ]);
+
+    $array = xmlConverter(replaceWords($document->getBody()));
+
+    echo $array['CADASTRAIS']['NOME'];
+    echo "|";
+    echo $array['CADASTRAIS']['NOMEMAE'];
+    echo "|";
+    echo $array['CADASTRAIS']['NASCIMENTO'];
+    echo "|";
+    echo $array['CADASTRAIS']['SEXO'];
+
+} elseif ($type == 'cnpj'){
+
+    $res = $client->request('GET', 'http://wsnv.novavidati.com.br/WSLocalizador.asmx/GerarToken?', [
+        'query'=> ['usuario'=> $user, 'senha' => $pass , 'cliente' => $customer]
+    ]);
+
+    $token = replaceWords($res->getBody());
+
+    $document = $client->request('GET', 'http://wsnv.novavidati.com.br/WSLocalizador.asmx/PessoasEmpresasTk?', [
+        'query'=> ['documento'=> $document_number, 'token' => $token]
+    ]);
+
+    $array = xmlConverter(replaceWords($document->getBody()));
+
+    echo $array['CADASTRAIS']['RAZAO'];
+    echo "|";
+    echo $array['CADASTRAIS']['NOME_FANTASIA'];
+    echo "|";
+    echo $array['CADASTRAIS']['DATA_ABERTURA'];
+    echo "|";
+    echo $array['CADASTRAIS']['DESC_NATJUR'];
+    echo "|";
+    echo $array['CADASTRAIS']['CNAE'];
+    echo "|";
+    echo $array['CADASTRAIS']['DESC_CNAE'];
+    echo "|";
+    echo $array['CADASTRAIS']['MATRIZ'];
+
+}
+
+
+
