4053af7bfe58351cd76c1c6c31f4be67215c821b
Marcos Santiago <marcos14952010@hotmail.com>
2017-08-10 01:31:58 -0300

    included the dotenv in api

--- index.php
+++ index.php
@@ -2,16 +2,21 @@
 
 require 'vendor/autoload.php';
 require 'helpers/commom.php';
-require 'config/config.php';
 
 use GuzzleHttp\Client;
+use Dotenv\Dotenv;
 
 $client = new GuzzleHttp\Client();
+
+$dotenv = new Dotenv( __DIR__ );
+$dotenv->load();
+
+echo $_ENV['cliente'];
 
 if($type == 'cpf'){
 
     $res = $client->request('GET', 'http://wsnv.novavidati.com.br/WSLocalizador.asmx/GerarToken?', [
-        'query'=> ['usuario'=> $user, 'senha' => $pass , 'cliente' => $customer]
+        'query'=> ['usuario'=> $_ENV['usuario'], 'senha' => $_ENV['senha'] , 'cliente' => $_ENV['cliente']]
     ]);
 
     $token = replaceWords($res->getBody());
